90b1ae928199f572c1e0e7ad632875c8932a9fd2
thefearlesscoder <thefunnelvkd@gmail.com>
2025-12-10 12:59:47 +0530

    complete backend

--- ai-ticket-backend/index.js
+++ ai-ticket-backend/index.js
@@ -2,7 +2,14 @@ import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
 import userRoutes from './routes/user.routes.js';
-import 
+import ticketRoutes from './routes/ticket.routes.js';
+import {serve} from "inngest/express";
+import { inngest } from './inngest/client.js';
+import { onUserSignup } from './inngest/functions/on-signup.js';
+import { onTicketCreated } from './inngest/functions/on-ticket-create.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 const app = express();
@@ -11,6 +18,12 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", userRoutes); // user routes
+app.use("/api/tickets", ticketRoutes); // ticket routes
+// route for inngest things to work
+app.use("/api/inngest", serve({
+    client: inngest, 
+    functions: [onUserSignup, onTicketCreated]
+}));
 
 // Connect to MongoDB
 mongoose
--- ai-ticket-backend/routes/ticket.routes.js
+++ ai-ticket-backend/routes/ticket.routes.js
@@ -1,9 +1,12 @@
+import { get } from "mongoose";
+import { createTicket, getTicket, getTickets } from "../controllers/ticket";
 import { authenticate } from "../middlewares/auth";
 import express from "express";
 
 const router  = express.Router();
 
-router.post("/create-ticket", )
-
+router.get("/", authenticate, getTickets);
+router.get("/:id", authenticate, getTicket);
+router.post("/", authenticate, createTicket);
 
 export default router;
